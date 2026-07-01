b10958bccbc83e1e6aee927bbd45a420b24ace74
fidelvilla02 <villafidel96@gmail.com>
2026-07-01 14:15:24 +0800

    added third num

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 function calculateSum(){
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
 
     const number1 = parseFloat(num1);
     const number2 = parseFloat(num2);
+    const number3 = parseFloat(num3);
 
-    const sum =number1 + number2;
-    document.getElementById('result').textContent='The sum is ' + sum;
+    const sum =number1 + number2 + number3;
+    document.getElementById('result').textContent='The sum is' + sum;
+
+  
+}
+function calculateMult() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
+
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+    const number3 = parseFloat(num3);
+
+    const mult = number1 * number2 * number3;
+
+    document.getElementById('result').textContent =
+        'The product is ' + mult;
+}
+function clearFields() {
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
+    document.getElementById('num3').value = "";
+    document.getElementById('result').textContent = "";
 }
